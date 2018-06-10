a6f6f9ac22f04b7d746484ef7a99f492221fd707
munirkizilcali <munir.kizilcali@gmail.com>
2018-06-10 11:51:48 -0400

    Done.

--- src/domController.js
+++ src/domController.js
@@ -1,3 +1,15 @@
-function updateDOM() {
-
+function updateDOM(ident) {
+	let rowID = 0
+	let columnID = 0
+	for (let i = 0; i < 15; i++) {
+		rowID = Math.floor(i/3)
+		columnID = i%3
+		if (ident.booleanMap()[i] === 1) {
+			document.getElementById(`${rowID}-${columnID}`).classList.add('fill')
+			document.getElementById(`${rowID}-${4-columnID}`).classList.add('fill')
+		} else {
+			document.getElementById(`${rowID}-${columnID}`).classList.remove('fill')
+			document.getElementById(`${rowID}-${4-columnID}`).classList.remove('fill')
+		}
+	}
 }
